2e43822f17afc59bbb18cd133755062c93936e74
Revanth-Mummidi <mummidirevanth@gmail.com>
2024-09-02 15:28:44 +0530

    .

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -5,55 +5,55 @@ import { debounceTime, Subject } from 'rxjs';
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
-
+ 
+  searchTxt:string='';
   @Input() set searchtext(e: any) {
     console.log("Change", e);
-
+    this.searchTxt=e;
+    this.removeHighlighting();
     this.highlight(e);
   };
 
   constructor(private el: ElementRef, private renderer: Renderer2) { };
 
 
-  private highlight(searchtext: string) {
+  private highlight (searchtext: string) {
+  
     const element = this.el.nativeElement;
-    console.log("Paragraph", element);
-    // this.getElementsWithTextContent(element);
-    const arr= this.getElementsWithTextContent(element);
-    if (arr.length > 0) {
-     arr.map((child: any) => {
-
-        this.applyHighlighting(child, searchtext);
-      });
-    } else {
-      this.applyHighlighting(element, searchtext);
-    }
+     this.findChildren(element,searchtext);
   }
 
-  private getElementsWithTextContent(element: HTMLElement): HTMLElement[] | any {
-    let textElements: HTMLElement[] = [];
-    const children = element.children;
-    for (let i = 0; i < children.length; i++) {
-      const child = children[i] as HTMLElement;
-      if (child.textContent && child.textContent.trim().length > 0) {
-        textElements.push(child);        
-      }
-      textElements = textElements.concat(
-        this.getElementsWithTextContent(child)
-      );
-    }
-    return textElements;
+  private findChildren(element : HTMLElement,searchTxt:string):void{
+    // console.log("FIND",element,searchTxt);
+    // console.log("CHILD",element,element.children,element.children.length);
+    if(element.children.length>0)
+    {
+    Array.from(element.children).forEach((child) => {
+        //  console.log("CHILDREN",child);
+            this.findChildren(child as HTMLElement,searchTxt);
+         
+         
+    });
+   
+  }
+  else{
+    // console.log("ROOT",element,searchTxt);
+    this.applyHighlighting(element,searchTxt);
+    // this.removeHighlighting();
+  }
   }
 
 
   private applyHighlighting(element: HTMLElement, searchtext: string) {
+    
     const text = element.textContent || '';
-
+    
     if (!searchtext) {
       this.removeHighlighting();
       return;
     }
-
+   
+    console.log("Search=",searchtext,text)
     const regex = new RegExp(`(${searchtext})`, 'gi');
     const newText = text.replace(regex, `<span style="background-color: #e5f674; color:black">$1</span>`);
     console.log("OUTs", newText);
